3bf18719f456cc52bad3933e7e07523f5255e7f6
Fatemah Al-Hajji Boukhadour <f.alhajji@gmail.com>
2019-07-09 21:09:43 +0300

    Django is interesting

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,24 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    	"my_list" :[
+   		#Restaurant 1
+    	{
+    	  "restaurant_name" : "Wahid Falafel",
+    	  "food_type" : "Middle Eastern cuisine"
+    	},
+		#Restaurant 2
+    	{
+    	  "restaurant_name" : "Katsuya",
+    	  "food_type" : "Asian Fusion"
+    	},
+    	#Restaurant 3
+    	{
+    	  "restaurant_name" : "Pizza Express",
+    	  "food_type" : "Italian cuisine"
+    	},
 
+    	]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +31,12 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    	"my_object" : {
+    	#Restaurant Detail
+    	  "restaurant_name" : "McDonald's",
+    	  "food_type" : "Burgers"
+    	
+    	}
 
     }
     return render(request, 'detail.html', context)
